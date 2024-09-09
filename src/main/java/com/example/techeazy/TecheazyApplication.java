1b7191b594c0c4e9afc0aa38f0020cc14275729b
[Saikumar] <[76saikumar@gmail.com]>
2024-09-09 09:03:54 +0530

    Initial commit

---
+++ src/main/java/com/example/techeazy/TecheazyApplication.java
@@ -0,0 +1,13 @@
+package com.example.techeazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecheazyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/techeazy/TecheazyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techeazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheazyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
